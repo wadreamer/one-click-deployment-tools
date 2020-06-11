86d51bf8e49db7eef9d8de5d5ed5359c5b2db774
lww <1013232063@qq.com>
2020-06-11 11:34:07 +0800

    2020年6月11日11:33:58

--- deploytools/src/main/java/com/cfg/deploytools/controller/FileController.java
+++ deploytools/src/main/java/com/cfg/deploytools/controller/FileController.java
@@ -99,4 +99,10 @@ public class FileController {
         return fileService.getFileContent(taskFile);
     }
 
+
+    @RequestMapping("/deployFile")
+    public AjaxResult deployFile(int fileId){
+        File deployFile = fileService.getFileById(fileId);
+        return deployFile != null ? AjaxResult.success(200, deployFile) : AjaxResult.error("操作失败，请稍后重试");
+    }
 }
